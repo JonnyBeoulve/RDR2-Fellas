f13b0b7a1e734dc5479a3880529526f25121f3da
Jonathan Leack <jleack@Jonathans-MacBook-Pro.local>
2018-10-18 19:06:52 -0700

    Resuming project using Mac laptop with Django 1.11

--- rdr2_fellas/urls.py
+++ rdr2_fellas/urls.py
@@ -5,16 +5,16 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, url
+    2. Add a URL to urlpatterns:  url('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from rdr2_fellas import views
 
 # Namespace
@@ -22,12 +22,12 @@ app_name = 'rdr2_fellas'
 
 # URL structure
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('partner/', views.partner, name='partner'),
-    path('myprofile/', views.my_profile, name='myprofile'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('register/', views.user_register, name='register'),
-    path('admin/', admin.site.urls)
+    url(r'^about/$', views.about, name='about'),
+    url(r'^partner/$', views.partner, name='partner'),
+    url(r'^myprofile/$', views.my_profile, name='myprofile'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^register/$', views.user_register, name='register'),
+    url(r'^admin/$', admin.site.urls),
+    url(r'', views.home, name='home'),
 ]
